613aa3919aecfe68c33d4e8d66f42b5d20566aec
SHINJAEUN <wodms1102@gmail.com>
2024-09-05 10:16:15 +0900

    상세 뷰 경로 수정

--- frontend/vue_js/src/views/fo/enterprise/resume/ResumeDetailView.vue
+++ frontend/vue_js/src/views/fo/enterprise/resume/ResumeDetailView.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        <SelfintoductionsResume />
-        dvdvdbd
+        <DetailResume />
+    </div>
+    <div class="col-lg-12">
+        <div class="float-end">
+            <router-link class="btn btn-outline btn-xl btn-light mb-2" to="/resumes/resume-list">확인</router-link>
+        </div>
     </div>
 </template>
 
 <script setup>
-import SelfintoductionsResume from '@/components/fo/enterprise/resume/SelfintoductionsResume.vue';
+import DetailResume from '../../../../components/fo/enterprise/resume/DetailResume.vue';
 
 </script>
 

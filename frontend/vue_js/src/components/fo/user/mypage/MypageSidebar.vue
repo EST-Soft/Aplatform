3c888a36c642085b71be5262bcfa768be60b0320
SHINJAEUN <wodms1102@gmail.com>
2024-09-24 10:56:11 +0900

    사이드바 수정

--- frontend/vue_js/src/components/fo/user/mypage/MypageSidebar.vue
+++ frontend/vue_js/src/components/fo/user/mypage/MypageSidebar.vue
@@ -32,7 +32,7 @@
     <div class="col-lg-3" v-if="isNavVisible">
         <button class="btn btn-arrow-effect-1 bg-transparent text-primary border-0 p-0 me-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling" style="margin-bottom: 30px;">MENU<i class="fas fa-arrow-right ms-2"></i></button>
 
-        <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" style="width: 30%;">
+        <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" style="width: 50%;">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasScrollingLabel">마이페이지</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -40,26 +40,26 @@
             <div class="offcanvas-body">
                 <aside class="sidebar" style="margin-bottom: 80px;">
                     <ul class="nav nav-list flex-column mb-5">
-                        <li class="nav-item"><router-link class="nav-link" to="/mypage/home" id="menu1">현황 및 일정</router-link></li>
-                        <li class="nav-item"><router-link class="nav-link" to="/resumes/resume-insert" id="menu2">이력서 관리</router-link>
+                        <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/home" id="menu1">현황 및 일정</router-link></li>
+                        <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/resumes/resume-insert" id="menu2">이력서 관리</router-link>
                             <ul>
-                                <li class="nav-item"><router-link class="nav-link" to="/resumes/resume-insert">이력서 등록</router-link></li>
-                                <li class="nav-item"><router-link class="nav-link" to="/resumes/resume-list">이력서 목록</router-link></li>
+                                <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/resumes/resume-insert">이력서 등록</router-link></li>
+                                <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/resumes/resume-list">이력서 목록</router-link></li>
                             </ul>
                         </li>
-                        <li class="nav-item"><router-link class="nav-link" to="/mypage/applycompanylist" id="menu3">입사지원현황</router-link></li>
-                        <li class="nav-item"><router-link class="nav-link" to="/mypage/ppAcception" id="menu4">포지션 제안</router-link>
+                        <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/applycompanylist" id="menu3">입사지원현황</router-link></li>
+                        <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/ppAcception" id="menu4">포지션 제안</router-link>
                             <ul>
-                                <li class="nav-item"><router-link class="nav-link" to="/mypage/ppAcception">포지션 제안 설정</router-link></li>
-                                <li class="nav-item"><router-link class="nav-link" to="/mypage/proposedPosition">제안 받은 포지션</router-link></li>
+                                <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/ppAcception">포지션 제안 설정</router-link></li>
+                                <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/proposedPosition">제안 받은 포지션</router-link></li>
                             </ul>
                         </li>
-                        <li class="nav-item"><router-link class="nav-link" to="/mypage/scrapList" id="menu5">스크랩 공고</router-link></li>
-                        <li class="nav-item"><router-link class="nav-link"  :to="{ name: 'userInfmtn', query: { modal: 'true' } }" id="menu6">회원정보 관리</router-link>
+                        <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/scrapList" id="menu5">스크랩 공고</router-link></li>
+                        <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link"  :to="{ name: 'userInfmtn', query: { modal: 'true' } }" id="menu6">회원정보 관리</router-link>
                             <ul>
-                                <li class="nav-item"><router-link class="nav-link" :to="{ name: 'userInfmtn', query: { modal: 'true' } }">회원정보 수정</router-link></li>
-                                <li class="nav-item"><router-link class="nav-link" to="/mypage/userPwChange">비밀번호 변경</router-link></li>
-                                <li class="nav-item"><router-link class="nav-link" to="/mypage/userSingout">회원탈퇴</router-link></li>
+                                <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" :to="{ name: 'userInfmtn', query: { modal: 'true' } }">회원정보 수정</router-link></li>
+                                <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/userPwChange">비밀번호 변경</router-link></li>
+                                <li class="nav-item" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><router-link class="nav-link" to="/mypage/userSingout">회원탈퇴</router-link></li>
                             </ul>
                         </li>
                     </ul>
